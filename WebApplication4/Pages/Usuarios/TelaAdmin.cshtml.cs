aff092f7ea233a217f5df219130d7c261f1e060c
1710492 <1710492@DASOE671C6>
2018-12-04 20:26:18 -0200

    C3 ou C5/C6 - Cria tela para incluir tipo de jogo #6

---
+++ WebApplication4/Pages/Usuarios/TelaAdmin.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication4.Pages.Usuarios
+{
+    public class TelaAdminModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
