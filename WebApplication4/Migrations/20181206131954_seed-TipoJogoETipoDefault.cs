991b7fc7c0d62d3443ffb0b2a4c9bac705b8d6c9
user <user@DESKTOP-UFTLGL2>
2018-12-06 12:43:22 -0200

    Insere o tipojogo xadrez, liga as mesas existentes com esse tipojogo e torna a fk entre mesas e tipojogo como not null

---
+++ WebApplication4/Migrations/20181206131954_seed-TipoJogoETipoDefault.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication4.Migrations
+{
+    public partial class seedTipoJogoETipoDefault : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"INSERT [dbo].[TipoJogo]
+                    ([Nome], [Url], [MinJogadores], [MaxJogadores])
+                    values
+                    ('Xadrez', '/Mesas/MesaChess', 2, 2);");
+            migrationBuilder.Sql(
+                //update TABLE1 set tabel2id = TABLE2.Id from TABLE1 s inner join TABLE2 s on u.name = s.NAME
+                @"UPDATE Mesas
+                    SET TipoJogoId=SCOPE_IDENTITY();");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebApplication4/Migrations/20181206143924_updateMesaRequiredJogoId.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication4.Migrations
+{
+    public partial class updateMesaRequiredJogoId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mesas_TipoJogo_TipoJogoId",
+                table: "Mesas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TipoJogoId",
+                table: "Mesas",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mesas_TipoJogo_TipoJogoId",
+                table: "Mesas",
+                column: "TipoJogoId",
+                principalTable: "TipoJogo",
+                principalColumn: "TipoJogoId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Mesas_TipoJogo_TipoJogoId",
+                table: "Mesas");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TipoJogoId",
+                table: "Mesas",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Mesas_TipoJogo_TipoJogoId",
+                table: "Mesas",
+                column: "TipoJogoId",
+                principalTable: "TipoJogo",
+                principalColumn: "TipoJogoId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
