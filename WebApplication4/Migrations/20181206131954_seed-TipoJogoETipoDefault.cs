1092e3b892ec9b58b13a008d744266f5a6f19e4f
user <user@DESKTOP-UFTLGL2>
2018-12-06 13:42:42 -0200

    Tentativa correção do migration para Postgresql

--- WebApplication4/Migrations/20181206131954_seed-TipoJogoETipoDefault.cs
+++ WebApplication4/Migrations/20181206131954_seed-TipoJogoETipoDefault.cs
@@ -8,14 +8,13 @@ namespace WebApplication4.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(
-                @"INSERT [dbo].[TipoJogo]
-                    ([Nome], [Url], [MinJogadores], [MaxJogadores])
+                @"INSERT TipoJogo
+                    (Nome, Url, MinJogadores, MaxJogadores)
                     values
                     ('Xadrez', '/Mesas/MesaChess', 2, 2);");
             migrationBuilder.Sql(
-                //update TABLE1 set tabel2id = TABLE2.Id from TABLE1 s inner join TABLE2 s on u.name = s.NAME
                 @"UPDATE Mesas
-                    SET TipoJogoId=SCOPE_IDENTITY();");
+                    SET TipoJogoId=(SELECT Min(TipoJogoId) FROM TipoJogo);");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
